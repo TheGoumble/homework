51284b077e47036a77698852225e7970433ce2e9
Javier Vargas <thebruh@Javiers-MacBook-Air.local>
2022-07-19 08:00:31 -0400

    hw 6 almost finished

--- day-six.js
+++ day-six.js
@@ -1,4 +1,21 @@
 // What are the seven paradigms of the database?
+// Key-value, Wide column, documnet, relational, graph, search, multi-model
 // Create a function that takes a base number and an exponent number and returns the calculation.
+function power(num, exponent) {
+  let x = 1
+  for (let i = 0; i <= exponent; i++) {
+    x *= num
+  }
+  console.log(x)
+}
+power(2, 2)
 // Create a function that takes an array and a string as arguments and returns the index of the string.
+function theDex(array, string) {}
+
 // Create a function that takes an array and returns the types of values (data types) in a new array.
+function dType(array) {
+  let dataT = []
+  for (let i = 0; i < array.length; i++) {
+    dataT.push(typeof array[i])
+  }
+}
